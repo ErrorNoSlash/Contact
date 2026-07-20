76e4e80b92b93f5d0709ad3dd03270e14260b999
dias <24575026-ErrorN0Slash@users.noreply.gitlab.com>
2026-07-20 23:59:45 +0200

    added site

--- scripts/alert.js
+++
@@ -1,8 +0,0 @@
-const websiteBtn = document.getElementById("website-btn");
-
-if (websiteBtn) {
-    websiteBtn.addEventListener("click", function (btn) {
-        btn.preventDefault();
-        alert("Still working on website :/");
-    });
-}
